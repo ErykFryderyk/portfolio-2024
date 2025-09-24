2d1fc21b949247e4a249f25d70f505f1d3796f9f
Eryk Fryderyk <eryk@example.com>
2025-09-24 23:08:31 +0200

    set basic router-links for the router

--- src/router/index.js
+++ src/router/index.js
@@ -7,15 +7,47 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      meta: {
+        enterClass: 'animate__animated animate__fadeInUp',
+        leaveClass: 'animate__animated animate__fadeOutUp'
+      },
       component: HomeView
     },
     {
       path: '/about',
       name: 'about',
+      meta: {
+        enterClass: 'animate__animated animate__fadeInUp',
+        leaveClass: 'animate__animated animate__fadeOutUp'
+      },
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      meta: {
+        enterClass: 'animate__animated animate__fadeInUp',
+        leaveClass: 'animate__animated animate__fadeOutUp'
+      },
+      // route level code-splitting
+      // this generates a separate chunk (Projects.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProjectsView.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      meta: {
+        enterClass: 'animate__animated animate__fadeInUp',
+        leaveClass: 'animate__animated animate__fadeOutUp'
+      }, 
+      // route level code-splitting
+      // this generates a separate chunk (Contact.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ContactView.vue')
     }
   ]
 })
